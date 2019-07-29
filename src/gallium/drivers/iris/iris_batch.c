61411c27ac1b6c747864ca0fbf53d44a4fae2ce2
Rafael Antognolli <rafael.antognolli@intel.com>
2019-07-29 11:24:45 -0700

    iris/resource: Allocate in system memory in some cases.
    When we are allocating a buffer that is expected to be read/written from
    the CPU multiple times, but read/written from the GPU only once, we
    should probably allocate it in system memory.

--- src/gallium/drivers/iris/iris_batch.c
+++ src/gallium/drivers/iris/iris_batch.c
@@ -355,7 +355,7 @@ create_batch(struct iris_batch *batch)
    struct iris_bufmgr *bufmgr = screen->bufmgr;
 
    batch->bo = iris_bo_alloc(bufmgr, "command buffer",
-                             BATCH_SZ + BATCH_RESERVED, IRIS_MEMZONE_OTHER, 0);
+                             BATCH_SZ + BATCH_RESERVED, IRIS_MEMZONE_OTHER, BO_ALLOC_CPU);
    batch->bo->kflags |= EXEC_OBJECT_CAPTURE;
    batch->map = iris_bo_map(NULL, batch->bo, MAP_READ | MAP_WRITE);
    batch->map_next = batch->map;
