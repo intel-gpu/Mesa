6d864a5b1f73339a9485cb524f71cd26f8de27f9
Rafael Antognolli <rafael.antognolli@intel.com>
2019-07-29 10:59:01 -0700

    iris/bufmgr: Add flag to allocate from local memory.

--- src/gallium/drivers/iris/iris_resource.c
+++ src/gallium/drivers/iris/iris_resource.c
@@ -919,7 +919,7 @@ iris_resource_create_for_buffer(struct pipe_screen *pscreen,
       name = "dynamic state";
    }
 
-   res->bo = iris_bo_alloc(screen->bufmgr, name, templ->width0, memzone);
+   res->bo = iris_bo_alloc(screen->bufmgr, name, templ->width0, memzone, 0);
    if (!res->bo) {
       iris_resource_destroy(pscreen, &res->base);
       return NULL;
@@ -1374,7 +1374,7 @@ iris_invalidate_resource(struct pipe_context *ctx,
    struct iris_bo *old_bo = res->bo;
    struct iris_bo *new_bo =
       iris_bo_alloc(screen->bufmgr, res->bo->name, resource->width0,
-                    iris_memzone_for_address(old_bo->gtt_offset));
+                    iris_memzone_for_address(old_bo->gtt_offset), 0);
    if (!new_bo)
       return;
 
