6cee85364d700cb3deace7205d6c24f788b216f7
Sagar Ghuge <sagar.ghuge@intel.com>
2020-04-02 20:16:08 -0700

    anv: Allocate BO in appropriate region
    Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>

--- src/intel/vulkan/anv_allocator.c
+++ src/intel/vulkan/anv_allocator.c
@@ -1619,6 +1619,12 @@ anv_device_alloc_bo(struct anv_device *device,
    if (!device->physical->has_implicit_ccs)
       assert(!(alloc_flags & ANV_BO_ALLOC_IMPLICIT_CCS));
 
+   /* Sets true if local memory is available and allocation flag is explicitly
+    * set to allocate buffer in local memory.
+    */
+   bool alloc_local_mem = device->physical->vram.size > 0 &&
+      (alloc_flags & ANV_BO_ALLOC_LOCAL_MEM);
+
    const uint32_t bo_flags =
       anv_bo_alloc_flags_to_bo_flags(device, alloc_flags);
    assert(bo_flags == (bo_flags & ANV_BO_CACHE_SUPPORTED_FLAGS));
@@ -1639,7 +1645,29 @@ anv_device_alloc_bo(struct anv_device *device,
       ccs_size = align_u64(DIV_ROUND_UP(size, GEN_AUX_MAP_GEN12_CCS_SCALE), 4096);
    }
 
-   uint32_t gem_handle = anv_gem_create(device, size + ccs_size);
+   uint32_t gem_handle;
+
+   /* If we have vram size, we have multiple memory regions and should choose
+    * one of them.
+    */
+   if (device->physical->vram.size > 0) {
+      struct drm_i915_gem_memory_class_instance regions[2];
+      uint32_t nregions = 0;
+
+      if (alloc_local_mem) {
+         /* For vram allocation, still use system memory as a fallback. */
+         regions[nregions++] = device->physical->vram.region;
+         regions[nregions++] = device->physical->sys.region;
+      } else {
+         regions[nregions++] = device->physical->sys.region;
+      }
+
+      gem_handle = anv_gem_create_ext(device, size + ccs_size, nregions,
+                                      (uintptr_t)regions);
+   } else {
+      gem_handle = anv_gem_create(device, size + ccs_size);
+   }
+
    if (gem_handle == 0)
       return vk_error(VK_ERROR_OUT_OF_DEVICE_MEMORY);
 
