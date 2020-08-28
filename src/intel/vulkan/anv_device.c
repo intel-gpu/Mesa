065ab87c3e7e55b3c50bc66ec8a7042fddf95e2e
Mark Janes <mark.a.janes@intel.com>
2020-08-28 13:01:53 -0700

    anv: Allocate buffers with write-combined local memory
    Marginally improves DG1 performance (< 1%)

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -2826,7 +2826,9 @@ static VkResult
 anv_device_init_hiz_clear_value_bo(struct anv_device *device)
 {
    VkResult result = anv_device_alloc_bo(device, "hiz-clear-value", 4096,
-                                         ANV_BO_ALLOC_MAPPED,
+                                         ANV_BO_ALLOC_MAPPED |
+                                         ANV_BO_ALLOC_LOCAL_MEM |
+                                         ANV_BO_ALLOC_WRITE_COMBINE,
                                          0 /* explicit_address */,
                                          &device->hiz_clear_bo);
    if (result != VK_SUCCESS)
@@ -3272,7 +3274,8 @@ VkResult anv_CreateDevice(
 
    result = anv_device_alloc_bo(device, "workaround", 4096,
                                 ANV_BO_ALLOC_CAPTURE | ANV_BO_ALLOC_MAPPED |
-                                ANV_BO_ALLOC_LOCAL_MEM /* flags */,
+                                ANV_BO_ALLOC_LOCAL_MEM |
+                                ANV_BO_ALLOC_WRITE_COMBINE /* flags */,
                                 0 /* explicit_address */,
                                 &device->workaround_bo);
    if (result != VK_SUCCESS)
