9a8aa0ece938c63a326f847e9183110fc5f4ef18
Anuj Phogat <anuj.phogat@gmail.com>
2021-06-02 14:03:50 -0700

    intel/mtl: Add L3 configurations
    Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>

--- src/intel/common/intel_l3_config.c
+++ src/intel/common/intel_l3_config.c
@@ -189,7 +189,9 @@ get_l3_list(const struct intel_device_info *devinfo)
       return &icl_l3_list;
 
    case 12:
-      if (intel_device_info_is_dg2(devinfo))
+      if (intel_device_info_is_mtl(devinfo))
+         return &dg2_l3_list;
+      else if (intel_device_info_is_dg2(devinfo))
          return &dg2_l3_list;
       else if (devinfo->platform == INTEL_PLATFORM_DG1 || devinfo->verx10 == 125)
          return &empty_l3_list;
