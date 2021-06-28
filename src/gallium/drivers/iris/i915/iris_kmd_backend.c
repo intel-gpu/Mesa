f78285d32650e1a29d65239951c9a5c0d57fed63
Jordan Justen <jordan.l.justen@intel.com>
2021-06-28 01:29:06 -0700

    iris: Use dii prelim drm-uapi interface
    Reworks:
     * Merge Jason's "iris: Gracefully fall back from DII to upstream"
     * Merge Ken's "iris: Fallback for DII which doesn't support
       I915_MMAP_OFFSET_FIXED"
     * Jordan: Add bufmgr->prelim_drm.
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/gallium/drivers/iris/i915/iris_kmd_backend.c
+++ src/gallium/drivers/iris/i915/iris_kmd_backend.c
@@ -30,6 +30,7 @@
 #include "dev/intel_debug.h"
 
 #include "drm-uapi/i915_drm.h"
+#include "drm-uapi/i915_drm_prelim.h"
 
 #include "iris/iris_bufmgr.h"
 #include "iris/iris_batch.h"
@@ -115,6 +116,7 @@ i915_gem_create(struct iris_bufmgr *bufmgr,
       .flags = 0,
    };
    if (alloc_flags & BO_ALLOC_PROTECTED) {
+      assert(!devinfo->prelim_drm);
       intel_i915_gem_add_ext(&create.extensions,
                              I915_GEM_CREATE_EXT_PROTECTED_CONTENT,
                              &protected_param.base);
@@ -130,9 +132,31 @@ i915_gem_create(struct iris_bufmgr *bufmgr,
                              &set_pat_param.base);
    }
 
-   if (intel_ioctl(iris_bufmgr_get_fd(bufmgr), DRM_IOCTL_I915_GEM_CREATE_EXT,
-                   &create))
-      return 0;
+   if (!devinfo->prelim_drm) {
+      if (intel_ioctl(iris_bufmgr_get_fd(bufmgr), DRM_IOCTL_I915_GEM_CREATE_EXT,
+                      &create))
+         return 0;
+   } else {
+      struct prelim_drm_i915_gem_object_param region_param = {
+         .size = ext_regions.num_regions,
+         .data = ext_regions.regions,
+         .param = PRELIM_I915_OBJECT_PARAM | PRELIM_I915_PARAM_MEMORY_REGIONS,
+      };
+      struct prelim_drm_i915_gem_create_ext_setparam setparam_region = {
+         .base = { .name = PRELIM_I915_GEM_CREATE_EXT_SETPARAM },
+         .param = region_param,
+      };
+      struct prelim_drm_i915_gem_create_ext prelim_create = {
+         .size = size,
+         .extensions = (uintptr_t)&setparam_region,
+      };
+
+      if (intel_ioctl(iris_bufmgr_get_fd(bufmgr),
+                      PRELIM_DRM_IOCTL_I915_GEM_CREATE_EXT,
+                      &prelim_create) != 0)
+         return 0;
+      create.handle = prelim_create.handle;
+   }
 
    if (iris_bufmgr_vram_size(bufmgr) == 0)
       /* Calling set_domain() will allocate pages for the BO outside of the
@@ -208,8 +232,16 @@ i915_gem_mmap_offset(struct iris_bufmgr *bufmgr, struct iris_bo *bo)
    }
 
    /* Get the fake offset back */
-   if (intel_ioctl(iris_bufmgr_get_fd(bufmgr), DRM_IOCTL_I915_GEM_MMAP_OFFSET,
-                   &mmap_arg)) {
+   int ret = intel_ioctl(iris_bufmgr_get_fd(bufmgr),
+                         DRM_IOCTL_I915_GEM_MMAP_OFFSET, &mmap_arg);
+   if (ret != 0 && mmap_arg.flags == I915_MMAP_OFFSET_FIXED) {
+      mmap_arg.flags =
+         iris_heap_is_device_local(bo->real.heap) ?
+         I915_MMAP_OFFSET_WC : I915_MMAP_OFFSET_WB;
+      ret = intel_ioctl(iris_bufmgr_get_fd(bufmgr),
+                        DRM_IOCTL_I915_GEM_MMAP_OFFSET, &mmap_arg);
+   }
+   if (ret != 0) {
       DBG("%s:%d: Error preparing buffer %d (%s): %s .\n",
           __FILE__, __LINE__, bo->gem_handle, bo->name, strerror(errno));
       return NULL;
