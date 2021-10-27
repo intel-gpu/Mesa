7cb39e6e9deb8863d9777814d77de4a28954e27c
Francisco Jerez <currojerez@riseup.net>
2021-10-26 17:18:58 -0700

    intel/dev: Read geometry topology with PRELIM_DRM_I915_QUERY_GEOMETRY_SLICES

--- src/intel/dev/i915/intel_device_info.c
+++ src/intel/dev/i915/intel_device_info.c
@@ -305,10 +305,15 @@ query_topology(struct intel_device_info *devinfo, int fd)
 
    if (devinfo->verx10 >= 125) {
       struct drm_i915_query_topology_info *geom_topo_info =
-         intel_i915_query_alloc(fd, DRM_I915_QUERY_GEOMETRY_SUBSLICES, NULL);
+         intel_i915_query_alloc(fd, PRELIM_DRM_I915_QUERY_GEOMETRY_SLICES,
+                                NULL);
       if (geom_topo_info == NULL) {
-         free(topo_info);
-         return false;
+         geom_topo_info =
+            intel_i915_query_alloc(fd, DRM_I915_QUERY_GEOMETRY_SUBSLICES, NULL);
+         if (geom_topo_info == NULL) {
+            free(topo_info);
+            return false;
+         }
       }
 
       update_from_single_slice_topology(devinfo, topo_info, geom_topo_info);
