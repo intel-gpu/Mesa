df59c0f5057c4784a71fde66009406ec8ebb6d53
Jordan Justen <jordan.l.justen@intel.com>
2022-04-15 01:07:29 -0700

    iris/resource: Disable upstream DG2 modifiers when using prelim_drm
    Reworks:
     * Marc: Allow I915_FORMAT_MOD_4_TILED to be supported on DII
     * Jordan: Don't advertise I915_FORMAT_MOD_4_TILED on DII since it's
       value has changed over time.
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/gallium/drivers/iris/iris_resource.c
+++ src/gallium/drivers/iris/iris_resource.c
@@ -118,7 +118,7 @@ modifier_is_supported(const struct intel_device_info *devinfo,
    case I915_FORMAT_MOD_4_TILED_DG2_RC_CCS:
    case I915_FORMAT_MOD_4_TILED_DG2_MC_CCS:
    case I915_FORMAT_MOD_4_TILED_DG2_RC_CCS_CC:
-      if (!intel_device_info_is_dg2(devinfo))
+      if (!intel_device_info_is_dg2(devinfo) || devinfo->prelim_drm)
          return false;
       break;
    case I915_FORMAT_MOD_4_TILED_MTL_RC_CCS:
@@ -282,6 +282,8 @@ iris_query_dmabuf_modifiers(struct pipe_screen *pscreen,
 
    for (int i = 0; i < ARRAY_SIZE(all_modifiers); i++) {
       if (!modifier_is_supported(devinfo, pfmt, 0, all_modifiers[i]))
+         continue;
+      if (all_modifiers[i] == I915_FORMAT_MOD_4_TILED && devinfo->prelim_drm)
          continue;
 
       if (supported_mods < max) {
