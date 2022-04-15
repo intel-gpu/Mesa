7e9eb236cc0041086714a4dae7988da76c551c76
Jordan Justen <jordan.l.justen@intel.com>
2022-04-15 01:08:10 -0700

    iris/resource: Support prelim DG2 modifiers when using prelim_drm
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/intel/isl/isl_drm.c
+++ src/intel/isl/isl_drm.c
@@ -150,6 +150,27 @@ isl_drm_modifier_info_list[] = {
       .supports_clear_color = true,
    },
    {
+      .modifier = PRELIM_I915_FORMAT_MOD_F_TILED_DG2_RC_CCS,
+      .name = "PRELIM_I915_FORMAT_MOD_F_TILED_DG2_RC_CCS",
+      .tiling = ISL_TILING_4,
+      .supports_render_compression = true,
+      .supports_clear_color = false,
+   },
+   {
+      .modifier = PRELIM_I915_FORMAT_MOD_F_TILED_DG2_MC_CCS,
+      .name = "PRELIM_I915_FORMAT_MOD_F_TILED_DG2_MC_CCS",
+      .tiling = ISL_TILING_4,
+      .supports_media_compression = true,
+      .supports_clear_color = false,
+   },
+   {
+      .modifier = PRELIM_I915_FORMAT_MOD_F_TILED_DG2_RC_CCS_CC,
+      .name = "PRELIM_I915_FORMAT_MOD_F_TILED_DG2_RC_CCS_CC",
+      .tiling = ISL_TILING_4,
+      .supports_render_compression = true,
+      .supports_clear_color = true,
+   },
+   {
       .modifier = I915_FORMAT_MOD_4_TILED_MTL_RC_CCS,
       .name = "I915_FORMAT_MOD_4_TILED_MTL_RC_CCS",
       .tiling = ISL_TILING_4,
@@ -245,11 +266,12 @@ isl_drm_modifier_get_score(const struct intel_device_info *devinfo,
          return 0;
 
       return 3;
+   case PRELIM_I915_FORMAT_MOD_F_TILED_DG2_RC_CCS:
    case I915_FORMAT_MOD_4_TILED_DG2_RC_CCS:
       if (!intel_device_info_is_dg2(devinfo))
          return 0;
 
-      if (devinfo->prelim_drm &&
+      if (devinfo->prelim_drm ==
           (modifier == I915_FORMAT_MOD_4_TILED_DG2_RC_CCS))
          return 0;
 
@@ -257,11 +279,12 @@ isl_drm_modifier_get_score(const struct intel_device_info *devinfo,
          return 0;
 
       return 4;
+   case PRELIM_I915_FORMAT_MOD_F_TILED_DG2_RC_CCS_CC:
    case I915_FORMAT_MOD_4_TILED_DG2_RC_CCS_CC:
       if (!intel_device_info_is_dg2(devinfo))
          return 0;
 
-      if (devinfo->prelim_drm &&
+      if (devinfo->prelim_drm ==
           (modifier == I915_FORMAT_MOD_4_TILED_DG2_RC_CCS_CC))
          return 0;
 
