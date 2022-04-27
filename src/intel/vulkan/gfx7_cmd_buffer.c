60babcfa9ca7bc3738a888f8df9e1630f61d38ce
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2022-04-27 23:44:40 +0300

    anv: rework sample location
    On Gfx7 we can only give the sample location for a given multisample
    number. This means everytime the multisampling value changes, we have
    to re-emit the locations. It's fine because it's also where
    (3DSTATE_MULTISAMPLE) the number of samples is stored.
    
    On Gfx8+ though, 3DSTATE_MULTISAMPLE only holds the number of samples
    and all the sample locations for all number of samples are located in
    3DSTATE_SAMPLE_PATTERN. So to be more effecient there, we need to
    track the locations for all sample numbers and compare new values with
    the relevant sample count when touching the dynamic state.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/intel/vulkan/gfx7_cmd_buffer.c
+++ src/intel/vulkan/gfx7_cmd_buffer.c
@@ -301,10 +301,13 @@ genX(cmd_buffer_flush_dynamic_state)(struct anv_cmd_buffer *cmd_buffer)
       anv_batch_emit_merge(&cmd_buffer->batch, dwords, pipeline->gfx7.wm);
    }
 
-   if (cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_DYNAMIC_SAMPLE_LOCATIONS) {
+   if (pipeline->base.device->vk.enabled_extensions.EXT_sample_locations &&
+       (cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_PIPELINE |
+                                       ANV_CMD_DIRTY_DYNAMIC_SAMPLE_LOCATIONS))) {
       genX(emit_multisample)(&cmd_buffer->batch,
                              pipeline->rasterization_samples,
-                             d->sample_locations.locations);
+                             anv_dynamic_state_get_sample_locations(d,
+                                                                    pipeline->rasterization_samples));
    }
 
    if (cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_DYNAMIC_COLOR_BLEND_STATE |
--- src/intel/vulkan/gfx8_cmd_buffer.c
+++ src/intel/vulkan/gfx8_cmd_buffer.c
@@ -622,10 +622,10 @@ genX(cmd_buffer_flush_dynamic_state)(struct anv_cmd_buffer *cmd_buffer)
    }
 #endif
 
-   if (cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_DYNAMIC_SAMPLE_LOCATIONS) {
+   if (pipeline->base.device->vk.enabled_extensions.EXT_sample_locations &&
+       cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_DYNAMIC_SAMPLE_LOCATIONS) {
       genX(emit_sample_pattern)(&cmd_buffer->batch,
-                                pipeline->rasterization_samples,
-                                d->sample_locations.locations);
+                                pipeline->rasterization_samples, d);
    }
 
    if (cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_PIPELINE |
