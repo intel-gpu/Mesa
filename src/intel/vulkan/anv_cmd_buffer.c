d06734ea79ab7b521d3d1055ea033e8e004774d7
Tapani Pälli <tapani.palli@intel.com>
2022-10-13 20:09:02 +0300

    anv: implement Wa_14015842950 for DG2
    According to WA description, we need to track DS write state
    and emit a PSS_STALL_SYNC whenever that state changes.
    
    v2: use a bitmask to track DS write state change (Lionel)
    v3: move to cmd_buffer and handle blorp (Lionel)
    
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>

--- src/intel/vulkan/anv_cmd_buffer.c
+++ src/intel/vulkan/anv_cmd_buffer.c
@@ -51,6 +51,7 @@ anv_cmd_state_init(struct anv_cmd_buffer *cmd_buffer)
    state->current_pipeline = UINT32_MAX;
    state->gfx.restart_index = UINT32_MAX;
    state->gfx.object_preemption = true;
+   state->ds_write_state = UINT8_MAX;
    state->gfx.dirty = 0;
 }
 
