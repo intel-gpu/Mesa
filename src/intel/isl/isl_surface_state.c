a34068359f5ce9a95f38ac661aa4574eead50b9d
Jianxun Zhang <jianxun.zhang@intel.com>
2023-01-09 13:12:54 -0800

    intel/isl: Add AUX MCS encoding into aux modes (xe2)
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

--- src/intel/isl/isl_surface_state.c
+++ src/intel/isl/isl_surface_state.c
@@ -83,7 +83,9 @@ static const uint32_t isl_encode_multisample_layout[] = {
 static const uint32_t isl_encode_aux_mode[] = {
    [ISL_AUX_USAGE_NONE] = AUX_NONE,
    [ISL_AUX_USAGE_MC] = AUX_NONE,
+   [ISL_AUX_USAGE_MCS] = AUX_MCS,
    [ISL_AUX_USAGE_MCS_CCS] = AUX_MCS,
+   [ISL_AUX_USAGE_STC_CCS] = AUX_NONE,
 };
 #elif GFX_VER >= 12
 static const uint32_t isl_encode_aux_mode[] = {
