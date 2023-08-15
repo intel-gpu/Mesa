596e654f725625c992581594e596e2aa0d462ab1
Tapani Pälli <tapani.palli@intel.com>
2023-08-15 12:33:36 +0300

    anv: implement required PSS stalls and barrier for Wa_14019039974
    NOTE: This hunk was dropped from this patch, and should be re-added if
    resource barriers are re-added.
    
    diff --git a/src/intel/vulkan/genX_cmd_buffer.c b/src/intel/vulkan/genX_cmd_buffer.c
    index 1c255ad5d1c..0803544a2f8 100644
    --- a/src/intel/vulkan/genX_cmd_buffer.c
    +++ b/src/intel/vulkan/genX_cmd_buffer.c
    @@ -1846,6 +1846,25 @@ static void anv_emit_barrier_for_type(struct anv_cmd_buffer *cmd_buffer,
                                           struct GENX(RESOURCE_BARRIER_BODY) body,
                                           const enum GENX(RESOURCE_BARRIER_TYPE) type)
     {
    +   /* Wa_14019039974 : After draw/mesh, if RESOURCE_BARRIER emitted, emit first:
    +    *
    +    * RESOURCE_BARRIER(Type: Immediate, Signal: Color, Wait: Pixel, Flush: None)
    +    *
    +    * Since the WA is A0 only, we do not track draw/mesh but just emit the
    +    * extra barrier.
    +    */
    +   if (intel_needs_workaround(cmd_buffer->device->info, 14019039974)) {
    +      struct GENX(RESOURCE_BARRIER_BODY) wa_body = {
    +         .BarrierType = RESOURCE_BARRIER_TYPE_IMMEDIATE,
    +         .SignalStage = RESOURCE_BARRIER_STAGE_COLOR,
    +         .WaitStage = RESOURCE_BARRIER_STAGE_PIXEL,
    +      };
    +      anv_batch_emit(&cmd_buffer->batch,
    +                  GENX(RESOURCE_BARRIER), barrier) {
    +         barrier.ResourceBarrierBody = wa_body;
    +      }
    +   }
    +
    
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Rohan Garg <rohan.garg@intel.com>

--- src/intel/vulkan/genX_cmd_buffer.c
+++ src/intel/vulkan/genX_cmd_buffer.c
@@ -2517,6 +2517,10 @@ genX(batch_emit_pipe_control_write)(struct anv_batch *batch,
    if (GFX_VER == 9 && (bits & ANV_PIPE_VF_CACHE_INVALIDATE_BIT))
       anv_batch_emit(batch, GENX(PIPE_CONTROL), pipe);
 
+   /* Wa_14019039974 : Set “PSS Sync Stall” for all PIPE_CONTROLs. */
+   if (intel_needs_workaround(devinfo, 14019039974))
+      bits |= ANV_PIPE_PSS_STALL_SYNC_BIT;
+
    anv_batch_emit(batch, GENX(PIPE_CONTROL), pipe) {
 #if GFX_VERx10 >= 125
       pipe.UntypedDataPortCacheFlushEnable =
