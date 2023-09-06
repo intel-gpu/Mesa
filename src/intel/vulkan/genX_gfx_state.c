e6708526029f4788138c21a97e97d8ee6fceb4f6
Tapani Pälli <tapani.palli@intel.com>
2023-09-06 08:01:25 +0300

    anv: send resource barrier on depth bounds change, Wa_14018283232
    Rework:
     * Rohan: Split out genX(emit_wa_14018283232) function
     * Rohan: Only use barrier if INTEL_DEBUG=usebarriers
    
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Signed-off-by: Rohan Garg <rohan.garg@intel.com>

--- src/intel/vulkan/genX_gfx_state.c
+++ src/intel/vulkan/genX_gfx_state.c
@@ -758,6 +758,12 @@ calculate_tile_dimensions(const struct anv_device *device,
    }                                                                   \
 
 ALWAYS_INLINE static void
+set_wa_14018283232(struct anv_gfx_dynamic_state *hw_state)
+{
+   SET(WA_14018283232, wa_14018283232_toggle, true);
+}
+
+ALWAYS_INLINE static void
 update_fs_msaa_flags(struct anv_gfx_dynamic_state *hw_state,
                      const struct vk_dynamic_graphics_state *dyn,
                      const struct anv_graphics_pipeline *pipeline)
@@ -1315,6 +1321,7 @@ update_depth_bounds(struct anv_gfx_dynamic_state *hw_state,
    if (dyn->ds.depth.bounds_test.enable) {
       SET(DEPTH_BOUNDS, db.DepthBoundsTestMinValue, dyn->ds.depth.bounds_test.min);
       SET(DEPTH_BOUNDS, db.DepthBoundsTestMaxValue, dyn->ds.depth.bounds_test.max);
+      set_wa_14018283232(hw_state);
    }
 }
 
