a1fce1ced9e64be4c688eb0eb1852d54c923836f
Rohan Garg <rohan.garg@intel.com>
2024-03-12 17:45:45 +0100

    anv: emit WA 14018283232 when toggling bits in 3DSTATE_PS_EXTRA
    WA 14018283232 indicates that we need to emit the resource barrier when
    toggling 3DSTATE_PS_EXTRA::Pixel Shader Kills Pixel amd
    3DSTATE_PS_EXTRA::Pixel Shader Valid.
    
    Signed-off-by: Rohan Garg <rohan.garg@intel.com>

--- src/intel/vulkan/genX_gfx_state.c
+++ src/intel/vulkan/genX_gfx_state.c
@@ -942,6 +942,7 @@ update_ps_extra_kills_pixel(struct anv_gfx_dynamic_state *hw_state,
                        (has_ds_feedback_loop(&fs_bin->bind_map, dyn) ||
                         wm_prog_data->uses_kill),
                        FRAGMENT);
+   set_wa_14018283232(hw_state);
 }
 
 #if GFX_VERx10 >= 125
@@ -2518,6 +2519,7 @@ cmd_buffer_gfx_state_emission(struct anv_cmd_buffer *cmd_buffer)
          SET(pse, ps_extra, EnablePSDependencyOnCPsizeChange);
 #endif
       }
+      set_wa_14018283232(hw_state);
    }
 
    if (BITSET_TEST(hw_state->dirty, ANV_GFX_STATE_CLIP)) {
