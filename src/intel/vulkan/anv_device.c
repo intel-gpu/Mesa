4861368328fe68442372dee1708e87a2098df7b7
Jianxun Zhang <jianxun.zhang@intel.com>
2024-04-17 22:41:01 -0700

    anv: Don't enable compression on external bos (xe2)
    Fix:
    
    dEQP-VK.synchronization.cross_instance.suballocated.
    write_draw_indexed_read_blit_image.image_128x128_r16
    _uint_binary_semaphore_fence_fd
    
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -4414,9 +4414,6 @@ VkResult anv_AllocateMemory(
    if (mem_type->propertyFlags & VK_MEMORY_PROPERTY_PROTECTED_BIT)
       alloc_flags |= ANV_BO_ALLOC_PROTECTED;
 
-   if (mem_type->compressed)
-      alloc_flags |= ANV_BO_ALLOC_COMPRESSED;
-
    /* For now, always allocated AUX-TT aligned memory, regardless of dedicated
     * allocations. An application can for example, suballocate a large
     * VkDeviceMemory and try to bind an image created with a CCS modifier. In
@@ -4479,6 +4476,9 @@ VkResult anv_AllocateMemory(
             alloc_flags |= ANV_BO_ALLOC_IMPLICIT_WRITE;
       }
    }
+
+   if (!(alloc_flags & ANV_BO_ALLOC_EXTERNAL) && mem_type->compressed)
+      alloc_flags |= ANV_BO_ALLOC_COMPRESSED;
 
    if (mem_type->descriptor_buffer)
       alloc_flags |= ANV_BO_ALLOC_DESCRIPTOR_BUFFER_POOL;
