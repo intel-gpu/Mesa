e1f915ce3b55d83acdf5d0d4cf223e8caba70454
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2025-10-01 11:32:09 +0300

    brw: fix render target indexing in FS output reads
    I forgot that the base indice is actually a more complex value that
    encodes the render target index and other things.
    
    Also fix the 1d-layered accesses by checking the size of the
    framebuffer.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14008
    Fixes: d4ab2087cf ("brw: lower non coherent FS load_output in NIR")

--- src/intel/compiler/brw_nir_lower_fs_load_output.c
+++ src/intel/compiler/brw_nir_lower_fs_load_output.c
@@ -20,27 +20,51 @@ brw_nir_lower_fs_load_output_instr(nir_builder *b,
 
    const struct brw_wm_prog_key *key = data;
 
+   const unsigned l = GET_FIELD(nir_intrinsic_base(intrin),
+                                BRW_NIR_FRAG_OUTPUT_LOCATION);
+   assert(l >= FRAG_RESULT_DATA0);
+   const unsigned load_offset = nir_src_as_uint(intrin->src[0]);
+   const unsigned target = l - FRAG_RESULT_DATA0 + load_offset;
+
    /* Only used by Iris that never sets this to SOMETIMES */
    assert(key->multisample_fbo != INTEL_SOMETIMES);
 
    b->cursor = nir_before_instr(&intrin->instr);
 
+   /* Query the framebuffer size to figure out where the layer index should go
+    * in the coordinates. RESINFO returns 0 in the 3rd component for 1D
+    * images.
+    */
+   nir_tex_instr *query = nir_tex_instr_create(b->shader, 0);
+   query->op = nir_texop_txs;
+   query->sampler_dim = GLSL_SAMPLER_DIM_3D;
+   query->is_array = true;
+   query->texture_index = target;
+   query->dest_type = nir_type_uint32;
+
+   nir_def_init(&query->instr, &query->def, 4, 32);
+   nir_builder_instr_insert(b, &query->instr);
+
    nir_def *coords[3] = {
       nir_f2u32(b, nir_channel(b, nir_load_frag_coord(b), 0)),
-      nir_f2u32(b, nir_channel(b, nir_load_frag_coord(b), 1)),
+      nir_bcsel(b,
+                nir_ieq_imm(b, nir_channel(b, &query->def, 2), 0),
+                nir_load_layer_id(b),
+                nir_f2u32(b, nir_channel(b, nir_load_frag_coord(b), 1))),
       nir_load_layer_id(b),
    };
-   nir_def *coord = nir_vec(b, coords, 3);
+   nir_def *coord = nir_vec(
+      b, coords, key->multisample_fbo == INTEL_NEVER ? 3 : 2);
 
    nir_def *tex =
       key->multisample_fbo == INTEL_NEVER ?
       nir_build_tex(b, nir_texop_txf, coord,
-                    .texture_index = nir_intrinsic_base(intrin),
+                    .texture_index = target,
                     .dim = GLSL_SAMPLER_DIM_2D,
                     .is_array = true,
                     .dest_type = nir_type_uint32) :
       nir_build_tex(b, nir_texop_txf_ms, coord,
-                    .texture_index = nir_intrinsic_base(intrin),
+                    .texture_index = target,
                     .ms_index = nir_load_sample_id(b),
                     .dim = GLSL_SAMPLER_DIM_MS,
                     .dest_type = nir_type_uint32);
