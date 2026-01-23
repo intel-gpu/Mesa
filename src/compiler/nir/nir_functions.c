3ab47bd7c9bf6ff27ddf1b3326fa9505fa513341
Ian Romanick <ian.d.romanick@intel.com>
2026-01-23 10:07:27 -0800

    nir: Use STACK_ARRAY instead of NIR_VLA
    The number of fields comes from the shader, so it could be a value large
    enough that using alloca would be problematic.
    
    Fixes: c11833ab24d ("nir,spirv: Rework function calls")
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
    Reviewed-by: Ryan Neph <ryanneph@google.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39866>
    (cherry picked from commit 9017d37e84771f921a63676dd8b955df9ef20f29)

--- src/compiler/nir/nir_functions.c
+++ src/compiler/nir/nir_functions.c
@@ -21,10 +21,10 @@
  * IN THE SOFTWARE.
  */
 
+#include "util/stack_array.h"
 #include "nir.h"
 #include "nir_builder.h"
 #include "nir_control_flow.h"
-#include "nir_vla.h"
 
 /*
  * TODO: write a proper inliner for GPUs.
@@ -171,12 +171,13 @@ static bool inline_functions_pass(nir_builder *b,
     * to an SSA value first.
     */
    const unsigned num_params = call->num_params;
-   NIR_VLA(nir_def *, params, num_params);
+   STACK_ARRAY(nir_def *, params, num_params);
    for (unsigned i = 0; i < num_params; i++) {
       params[i] = call->params[i].ssa;
    }
 
    nir_inline_function_impl(b, call->callee->impl, params, NULL);
+   STACK_ARRAY_FINISH(params);
    return true;
 }
 
